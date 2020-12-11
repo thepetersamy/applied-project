15cb0a6622feef7b18b7d2f5db8a33829610dd60
Peter <60013945+peter592@users.noreply.github.com>
2020-12-11 19:54:45 +0200

    Update Source.cpp

--- Graphics/Graphics/Source.cpp
+++ Graphics/Graphics/Source.cpp
@@ -15,6 +15,8 @@ int main()
 	g.setFullScreenMode();
 	g.hideCursor();
 
+	// getting height and width of screen
+	// y and x board are for calibrating the game board at the center of the screen
 	int height = g.getWindowHeight(), yBoard = height / 2 - 400;
 	int width = g.getWindowWidth(), xBoard = width / 2 - 400;
 
@@ -22,13 +24,17 @@ int main()
 	while (true)
 	{
 		g.beginDraw();
-		g.setDrawingColor(COLORS::WHITE);
+		
+		//setting color of background
 		g.fillScreen(COLORS::WHITE);
+		
 		g.setDrawingColor(COLORS::BLACK);
 		g.setDrawingThickness(15);
-		g.drawSolidRectangle(xBoard, yBoard, 800, 800); //boarder
+		g.drawSolidRectangle(xBoard, yBoard, 800, 800); //boarder of gameboard
+		
 		g.setDrawingThickness(0);
 
+		//drawing whtie and black tiles
 		for (int j = yBoard, count = 1; j < yBoard + 800; j = j + 100)
 		{ 			
 			for (int i = xBoard; i < xBoard + 800; i = i + 200) {
