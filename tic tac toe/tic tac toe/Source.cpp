8899211a118b60be9ce6460c15795bb3342e5a43
peter <petersamy592@gmail.com>
2020-12-18 22:58:42 +0200

    add one player feature by generating random numbers

--- tic tac toe/tic tac toe/Source.cpp
+++ tic tac toe/tic tac toe/Source.cpp
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #define SIZE 3
 
 void createGameBoard(int gameBoard[SIZE][SIZE]) {
@@ -176,18 +178,17 @@ void twoPlayerGame(int gameBoard[SIZE][SIZE]) {
 
 }
 
-
-void computersPlay(int gameBoard[SIZE][SIZE], int &x, int &y) {
-
+bool playerAboutToWinHorizontally(int gameBoard[SIZE][SIZE], int *rowNumber) {
 	// is real player about to win horizontally?
 	int counter = 0;
-	for (int row = 0; row < SIZE; row++) {
-		for (int column = 0; column < SIZE; column++) {
-			if (gameBoard[row][column] == 1) {
+	for (int column = 0; column < SIZE; column++) {
+		for (int row = 0; row< SIZE; row++) {
+			if (gameBoard[row][column] == 2) {
 				counter++;
 
 				if (counter == 2) {
-
+					rowNumber = &row;
+					return 1;
 				}
 			}
 			else {
@@ -195,7 +196,46 @@ void computersPlay(int gameBoard[SIZE][SIZE], int &x, int &y) {
 			}
 		}
 	}
+	return 0;
+}
 
+bool playerAboutToWinvertically(int gameBoard[SIZE][SIZE]) {
+	// is real player about to win horizontally?
+	int counter = 0;
+	for (int row = 0; row < SIZE; row++) {
+		for (int column = 0; column < SIZE; column++) {
+			if (gameBoard[row][column] == 2) {
+				counter++;
+
+				if (counter == 2) {
+					return 1;
+				}
+			}
+			else {
+				counter = 0;
+			}
+		}
+	}
+	return 0;
+}
+
+void computersPlay(int gameBoard[SIZE][SIZE], int *x, int *y) {
+	int rowNumber;
+	if (playerAboutToWinHorizontally(gameBoard, &rowNumber)) {
+
+	}
+
+
+}
+
+
+
+void generateRandomCordinates(int gameBoard[SIZE][SIZE], int* x, int* y) {
+	time_t t;
+	srand((unsigned) time(&t));
+	//printf("%d\n", rand() % 3);
+	
+	while (isOccupied(gameBoard, *x = rand() % 3, *y = rand() % 3));
 }
 
 void onePlayerGame(int gameBoard[SIZE][SIZE]) {
@@ -214,13 +254,20 @@ void onePlayerGame(int gameBoard[SIZE][SIZE]) {
 
 		int x, y;
 
-		// user input for column
-		printf("**PLAYER %d**\n", playersTurn);
-		printf("plaese enter the (x,y) coordinates you wish to place the token in:");
-		scanf("%d%d", &x, &y);
-		printf("\n\n");
 
+		if (playersTurn == 1) {
+			// user input for column
+			printf("**PLAYER %d**\n", playersTurn);
+			printf("plaese enter the (x,y) coordinates you wish to place the token in:");
 
+			scanf("%d%d", &x, &y);
+			printf("\n\n");
+
+		}
+		else if (playersTurn) {
+			generateRandomCordinates(gameBoard, &x, &y);
+			printf("computer played at (%d,%d)\n\n", x , y );
+		}
 		if (isBoardFull) {
 			if (coordinatesExist(x, y)) {
 
@@ -230,7 +277,7 @@ void onePlayerGame(int gameBoard[SIZE][SIZE]) {
 					printGameBoard(gameBoard);
 					if (playerWonVertically(gameBoard, playersTurn) || playerWonHorizontally(gameBoard, playersTurn) || playerWondiagonally(gameBoard, playersTurn)) {
 
-						printf("player %d WON!!!", playersTurn);
+						printf("COMPUTER WON!!!!");
 						break;
 					}
 
@@ -262,11 +309,24 @@ void onePlayerGame(int gameBoard[SIZE][SIZE]) {
 
 
 int main() {
+	int x, y;
+
 
 	int gameBoard[SIZE][SIZE];    // = { {2,2,2}, {2,2,2}, {2,2,2} };
+	int playerChoice;
 
-	twoPlayerGame(gameBoard);
+	
 
+	printf("please enter 1 or 2 for a sigleplayer or multiplayer game: ");
+	scanf("%d",&playerChoice);
+
+	if (playerChoice == 2) {
+		twoPlayerGame(gameBoard);
+	}
+	else if (playerChoice == 1) {
+		onePlayerGame(gameBoard);
+	}
+	
 
 	return 0;
 }
